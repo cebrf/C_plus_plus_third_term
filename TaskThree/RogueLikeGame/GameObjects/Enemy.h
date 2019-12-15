8e8bb53ac886e64ac84610c31434e4c727f3099e
cebrf <cebrf@yandex.ru>
2019-12-15 12:23:52 +1000

    T3 renaming project

--- TaskThree/RogueLikeGame/GameObjects/Enemy.h
+++ TaskThree/RogueLikeGame/GameObjects/Enemy.h
@@ -5,9 +5,6 @@ class Enemy :
     public ICharacter
 {
 public:
-    /*Enemy() = default;
-    Enemy(const Enemy& other) = default;*/
-
     Enemy(Point pos, char sym, int hp, int damage, int maxHp);
     ~Enemy();
 
